fbff16da673405ed3d51dcd6d96889b49f32436c
Tsup27 <72022909+Tsup27@users.noreply.github.com>
2021-01-02 14:46:37 +0530

    uploaded

---
+++ js/script.js
@@ -0,0 +1,21 @@
+function checkAnswer(e){
+    let parent= e.target.parentElement;
+    var inputElements = parent.querySelectorAll("input[type=radio]");
+    let explanation = parent.querySelector("p.hidden")
+    if(explanation) {
+        console.log(explanation)
+        explanation.classList.remove('hidden')
+    }
+    // console.log(parent)
+    for(element of inputElements) {
+        element.nextElementSibling.className = "";
+        if(element.checked == true) {
+            element.nextElementSibling.className = "wrong";
+        }
+        if(element.value == 'true'){
+            console.log('here')
+            console.log(element.nextElementSibling)
+            element.nextElementSibling.className = "correct";
+        }
+    }
+}
