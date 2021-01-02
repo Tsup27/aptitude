af0dfd39c13d547ddd79d95f0b0255f21c89d63f
Tsup27 <72022909+Tsup27@users.noreply.github.com>
2021-01-02 15:16:04 +0530

    option fix

--- js/script.js
+++ js/script.js
@@ -1,21 +1,20 @@
-function checkAnswer(e){
-    let parent= e.target.parentElement;
-    var inputElements = parent.querySelectorAll("input[type=radio]");
-    let explanation = parent.querySelector("p.hidden")
-    if(explanation) {
-        console.log(explanation)
-        explanation.classList.remove('hidden')
+function checkAnswer(e) {
+  let parent = e.target.parentElement;
+  var inputElements = parent.querySelectorAll("input[type=radio]");
+  let explanation = parent.querySelector("p.hidden");
+  if (explanation) {
+    explanation.classList.remove("hidden");
+  }
+  // console.log(parent)
+  for (element of inputElements) {
+    element.nextElementSibling.className = "";
+    if (element.checked == true) {
+      element.nextElementSibling.className = "wrong";
     }
-    // console.log(parent)
-    for(element of inputElements) {
-        element.nextElementSibling.className = "";
-        if(element.checked == true) {
-            element.nextElementSibling.className = "wrong";
-        }
-        if(element.value == 'true'){
-            console.log('here')
-            console.log(element.nextElementSibling)
-            element.nextElementSibling.className = "correct";
-        }
+    if (element.value == "true") {
+      console.log("here");
+      console.log(element.nextElementSibling);
+      element.nextElementSibling.className = "correct";
     }
+  }
 }
